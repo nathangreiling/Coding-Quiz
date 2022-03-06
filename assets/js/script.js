27b49b35fd6442468ddb1571a1b56f257942dd6b
nathangreiling <ngreiling20@gmail.com>
2022-03-06 16:14:14 -0700

    styling and interactions
    created some styling for the webpage using CSS. Also targeted some elements using JS  to make the page interactive.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-//start
+var startBtn = document.querySelector(".start-btn")
+var submitInitials_btn = document.querySelector(".submitInitials")
+var restart_btn = document.querySelector(".restart")
+
+startBtn.onclick = () =>{
+    starting.remove(".starting");
+
+}
+
